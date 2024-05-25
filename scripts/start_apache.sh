aa6abebd1baf9997a06c9cdce1af59f70a9abfd1
Devopsawsr <133389232+Devopsawsr@users.noreply.github.com>
2024-05-25 10:47:33 +0530

    Update start_apache.sh

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Deployment
-if [ "$DEPLOYMENT_GROUP_NAME" == "mnptecg-deploy" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "mnptech-dev-dg" ]
 then
 sudo cp -r /var/www/html/code-dir/* /var/www/html/
 sudo service httpd restart
